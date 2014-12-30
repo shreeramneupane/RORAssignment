c462adcea70d932943ffb576180b58e31d74faf9
prakash <prakashpoudel@lftechnology.com>
2014-12-30 16:53:38 +0545

    question 2 corrected for undistributable data

--- Day2/question2.rb
+++ Day2/question2.rb
@@ -5,7 +5,7 @@ def distribute(diamondArray)
     sum += diamondArray[i]
   end
   diamondShare = sum/3 #number of diamond to share to each person
-  if (sum%3 != 0 || diamondArray.length < 3)
+  if (sum%3 != 0 || diamondArray.max > diamondShare)
     puts "diamonds cannot be distributed equally"
   else
     for i in 0..2
@@ -33,4 +33,5 @@ def assignDiamond(diamondShare,personalDiamond,diamondArray)
   end
 end
 distribute ([1,2,3,4,5,6,3,3,3,6])
-#distribute ([1,2,3,4,5])
+# distribute ([1,2,3,4,5])
+# distribute ([1,2,3,4,5,232,3243,23423])
